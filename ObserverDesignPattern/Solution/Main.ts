a7f0b893c8370da63c7e31b821bc855d439d8f11
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-12 10:23:50 +0600

    Done!

--- ObserverDesignPattern/Solution/Main.ts
+++ ObserverDesignPattern/Solution/Main.ts
@@ -10,19 +10,16 @@ let data: Data = new Data()
 let fetcher: Fetcher = new Fetcher([student, teacher], data)
 
 
-console.log("Try 1:");
 data.setCourseNumber("SWE 4501")
 data.setTopicName("Observer Pattern")
 data.setTeacherName("Nazmul Haque")
-fetcher.fetch();
-student.show();
-teacher.show();
+fetcher.fetch()
+student.show()
+teacher.show()
 
-
-console.log("Data Changing");
 data.setCourseNumber("SWE 4502")
 data.setTopicName("Lab 01")
 data.setTeacherName("Mamun Rashid")
-fetcher.fetch();
-student.show();
-teacher.show();
+fetcher.fetch()
+student.show()
+teacher.show()
