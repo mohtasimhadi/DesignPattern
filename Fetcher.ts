7d635f4e46b0448d6b416400e0870eb85050d93c
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-12 09:47:03 +0600

    Created fetcher

---
+++ Fetcher.ts
@@ -0,0 +1,18 @@
+import Data from "./Data";
+import { Observer } from "./Observer";
+
+class Fetcher {
+    data: Data
+    observers: Observer[]
+
+    constructor(observers: Observer[], data: Data){
+        this.observers = observers
+        this.data = data
+    }
+
+    fetch = () => {
+        this.observers.forEach(observer => {
+            observer.update(this.data)
+        })
+    }
+}
