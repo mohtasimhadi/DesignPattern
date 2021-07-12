e6c9f0cc799af0151bef495045644121217fd1ea
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-12 09:28:12 +0600

    creating data files and generating getter and setter

---
+++ Data.ts
@@ -0,0 +1,39 @@
+class Data {
+    private courseNumber: string
+    private topicName: string
+    private teacherName: string
+    private time: string
+
+    public getCourseNumber(): string {
+        return this.courseNumber;
+    }
+
+    public setCourseNumber(courseNumber: string): void {
+        this.courseNumber = courseNumber;
+    }
+
+    public getTopicName(): string {
+        return this.topicName;
+    }
+
+    public setTopicName(topicName: string): void {
+        this.topicName = topicName;
+    }
+
+    public getTeacherName(): string {
+        return this.teacherName;
+    }
+
+    public setTeacherName(teacherName: string): void {
+        this.teacherName = teacherName;
+    }
+
+    public getTime(): string {
+        return this.time;
+    }
+
+    public setTime(time: string): void {
+        this.time = time;
+    }
+
+}
