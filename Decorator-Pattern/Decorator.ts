d5d76cb11bdb8325b86c9dcdb8d2a6b4487f5103
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-17 04:19:30 +0600

    text interface

---
+++ Decorator-Pattern/Decorator.ts
@@ -0,0 +1,14 @@
+import Text from "./Text"
+
+abstract class Decorator implements Text {
+    
+    text: Text
+
+    constructor(text : Text) {
+        this.text = text
+    }
+
+    getModifiedText(): string {
+        return this.text.getModifiedText()
+    }
+}
