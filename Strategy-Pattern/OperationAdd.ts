73361fe3542590219540105b74cbb0929648eb1a
mohtasimhadi <mohtasimhadi@gmail.com>
2021-08-20 22:11:26 +0600

    Strategy Pattern

--- Strategy-Pattern/OperationAdd.ts
+++ Strategy-Pattern/OperationAdd.ts
@@ -1,7 +1,7 @@
 import { Strategy } from "./Strategy"
 
 class OperationAdd implements Strategy {
-    doOperation(num1: BigInteger, num2: BigInteger): BigInteger {
+    doOperation(num1: number, num2: number): number {
         return num1 + num2
     }
 }
