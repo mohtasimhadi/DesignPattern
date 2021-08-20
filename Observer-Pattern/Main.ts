90b5572cd9452114663998e5ce1103c9290ee3d6
mohtasimhadi <mohtasimhadi@gmail.com>
2021-08-20 22:44:10 +0600

    Observer Pattern

--- Observer-Pattern/Main.ts
+++ Observer-Pattern/Main.ts
@@ -10,9 +10,9 @@ subject.attach(observer1);
 const observer2 = new ConcreteObserverB();
 subject.attach(observer2);
 
-subject.someBusinessLogic();
-subject.someBusinessLogic();
+// subject.someBusinessLogic();
+// subject.someBusinessLogic();
 
 subject.detach(observer2);
 
-subject.someBusinessLogic();
+// subject.someBusinessLogic();
